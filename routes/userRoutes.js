7d0ce7da171883405ba07db3b4860dfd278fb56e
Fatine EL HARIRI <hariri.fatine17@gmail.com>
2022-05-12 20:29:41 +0100

    correction of dependencies

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,21 @@
 //Contenir la logique routing
 const express = require('express');
 const router = express.Router();
-const stuffCtrl = require('../controllers/userCrud.js');
+const userCrud = require('../controllers/userCrud.js');
 
 //Indiquer à l'application la fonction à executer quand elle reçoit une requete http.
 
 //get all users
-router.get('/' ,stuffCtrl.getUsers);
+router.get('/' ,userCrud.getUsers);
 
 //updating
-router.put('/', stuffCtrl.updateUser)
+router.put('/', userCrud.updateUser)
 
 //Adding a user
-router.post('/', stuffCtrl.addUser);
+router.post('/', userCrud.addUser);
 
 //Deleting a user
-router.delete('/', stuffCtrl.deletUser);
+router.delete('/', userCrud.deletUser);
 
 
 module.exports = router;
