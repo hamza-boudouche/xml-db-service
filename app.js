8bf9f57a735865da8de9c39d8a370fa859dce374
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-05-20 04:02:18 +0100

    i feel sleepy

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require("cors")
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
 app.use('/api/projects', projectRoutes);
 
+app.use(express.static('out'));
 
 module.exports = app; 
--- utils/xml/xslfo.js
+++ utils/xml/xslfo.js
@@ -4,17 +4,15 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const generatePdf = (xmlFile, xslFile, outputFile) => {
-	// console.log(`fop -xml ${path.join(__dirname, `../../out/xml/${xmlFile}`)} -xsl ${path.join(__dirname, `./xsl/${xslFile}`)} -pdf ${path.join(__dirname, `../../out/pdf/${outputFile}`)}`)
-	const ls = spawn('powershell.exe', [String.raw`fop -xml C:\\Users\\Boudouche Hamza\\Desktop\\basex npm\\out\\xml\\something.xml -xsl C:\\Users\\Boudouche Hamza\\Desktop\\basex npm\\utils\\xml\\xsl\\projects.xsl -pdf C:\\Users\\Boudouche Hamza\\Desktop\\basex npm\\out\\pdf\\output.pdf`], { shell: true });
-	console.log("i am here")
-	return new Promise((resolve, reject) => {
-		ls.on('error', (error) => {
-			reject(error);
-		});
-		ls.on("close", code => {
-			resolve(code);
-		});
+	exec(`fop -xml "/mnt/c/Users/Boudouche\ Hamza/Desktop/basex\ npm/out/xml/${xmlFile}" -xsl "/mnt/c/Users/Boudouche\ Hamza/Desktop/basex\ npm/utils/xml/xsl/${xslFile}" -pdf "/mnt/c/Users/Boudouche\ Hamza/Desktop/basex\ npm/out/pdf/${outputFile}"`, (error, stdout, stderr) => {
+		if (error) {
+			console.log(error.message);
+		}
+		if (stderr) {
+			console.log(stderr)
+		}
 	})
+	console.log(true);
 }
 
 const saveXmlFile = async (xml, outputFile) => {
@@ -23,9 +21,8 @@ const saveXmlFile = async (xml, outputFile) => {
 }
 
 const clearOutFolders = () => {
-	console.log("hello world")
 	return new Promise((resolve, reject) => {
-		exec(`del ${path.join(__dirname, "../../out/pdf/*")} ; del ${path.join(__dirname, "../../out/xml/*")}`, (error, stdout, stderr) => {
+		exec(`rm ${path.join(__dirname, "../../out/pdf/*")} ; rm ${path.join(__dirname, "../../out/xml/*")}`, (error, stdout, stderr) => {
 			if (error) {
 				reject(error.message);
 			}
@@ -49,8 +46,8 @@ const sendPdf = (res, fileName) => {
 
 (async () => {
 	// saveXmlFile("something", "something.xml");
-	const res = await generatePdf("something.xml", "projects.xsl", "output.pdf");
-	console.log(res)
+	// const res = await generatePdf("something.xml", "projects.xsl", "output.pdf");
+	// clearOutFolders();
 })();
 
 module.exports = { generatePdf, saveXmlFile, clearOutFolders, sendPdf }
