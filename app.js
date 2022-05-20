92c586386603533df6cc7ef2e62561621fa7a6f9
Fatine EL HARIRI <hariri.fatine17@gmail.com>
2022-05-20 08:41:45 +0100

    cross implementation

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use('/api/users', userRoutes);
 
